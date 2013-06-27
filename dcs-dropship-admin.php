b6b21dbd33223db58697343fdad0014ca82a6b4d
Jason Douglas <jdouglas71@gmail.com>
2013-06-27 14:32:08 -0700

    Dashboard cleanup.

--- dcs-dropship-admin.php
+++ dcs-dropship-admin.php
@@ -75,17 +75,19 @@
 	
 	<form name="dcs_dropship_form" method="post" action="<?php echo str_replace( '%7E', '~', $_SERVER['REQUEST_URI']); ?>">
 		<input type="hidden" name="dcs_dropship_hidden" value="Y">
-		<p><?php _e("Inventory Data URL: " ); ?><input type="text" name="dcs_dropship_inventory_data_url" value="<?php echo $dcs_dropship_inventory_data_url; ?>" size="128"></p>
-		<p><?php _e("Product Data URL: " ); ?><input type="text" name="dcs_dropship_product_data_url" value="<?php echo $dcs_dropship_product_data_url; ?>" size="128"></p>
-		<p><?php _e("Orders In URL: " ); ?><input type="text" name="dcs_dropship_orders_url" value="<?php echo $dcs_dropship_orders_url; ?>" size="128"></p>
-		<p><?php _e("Order Status Data URL: " ); ?><input type="text" name="dcs_dropship_order_status_data_url" value="<?php echo $dcs_dropship_order_status_data_url; ?>" size="128"></p>
-		<p><?php _e("Tracking Data URL: " ); ?><input type="text" name="dcs_dropship_tracking_data_url" value="<?php echo $dcs_dropship_tracking_data_url; ?>" size="128"></p>
-		<p><?php _e("Order Invoice Data URL: " ); ?><input type="text" name="dcs_dropship_order_invoice_data_url" value="<?php echo $dcs_dropship_order_invoice_data_url; ?>" size="128"></p>
-		<p><?php _e("Dropship FTP username: " ); ?><input type="text" name="dcs_dropship_ftp_user" value="<?php echo $dcs_dropship_ftp_user; ?>" size="64"></p>
-		<p><?php _e("Dropship FTP password: " ); ?><input type="text" name="dcs_dropship_ftp_password" value="<?php echo $dcs_dropship_ftp_password; ?>" size="64"></p>
-		<p><?php _e("Dropship Shopping Cart Page: " ); ?><input type="text" name="dcs_dropship_shopping_cart_page" value="<?php echo $dcs_dropship_shopping_cart_page; ?>" size="128"></p>
-		<p><?php _e("Markup (in percentage): " ); ?><input type="text" name="dcs_dropship_markup" value="<?php echo $dcs_dropship_markup; ?>" size="2">%</p> 
-		<!-- <p><?php _e(": " ); ?><input type="text" name="dcs_dropship_" value="<?php echo $dcs_dropship_; ?>" size="128"></p> -->
+		<table>
+		<!-- <tr><td><?php _e("Inventory Data URL" ); ?></td><td><input type="text" name="dcs_dropship_inventory_data_url" value="<?php echo $dcs_dropship_inventory_data_url; ?>" size="128"></td></tr>              -->
+		<!-- <tr><td><?php _e("Product Data URL" ); ?></td><td><input type="text" name="dcs_dropship_product_data_url" value="<?php echo $dcs_dropship_product_data_url; ?>" size="128"></td></tr>                    -->
+		<!-- <tr><td><?php _e("Orders In URL" ); ?></td><td><input type="text" name="dcs_dropship_orders_url" value="<?php echo $dcs_dropship_orders_url; ?>" size="128"></td></tr>                                   -->
+		<!-- <tr><td><?php _e("Order Status Data URL" ); ?></td><td><input type="text" name="dcs_dropship_order_status_data_url" value="<?php echo $dcs_dropship_order_status_data_url; ?>" size="128"></td></tr>     -->
+		<!-- <tr><td><?php _e("Tracking Data URL" ); ?></td><td><input type="text" name="dcs_dropship_tracking_data_url" value="<?php echo $dcs_dropship_tracking_data_url; ?>" size="128"></td></tr>                 -->
+		<!-- <tr><td><?php _e("Order Invoice Data URL" ); ?></td><td><input type="text" name="dcs_dropship_order_invoice_data_url" value="<?php echo $dcs_dropship_order_invoice_data_url; ?>" size="128"></td></tr>  -->
+		<tr><td><?php _e("Dropship FTP username" ); ?></td><td><input type="text" name="dcs_dropship_ftp_user" value="<?php echo $dcs_dropship_ftp_user; ?>" size="64"></td></tr>
+		<tr><td><?php _e("Dropship FTP password" ); ?></td><td><input type="text" name="dcs_dropship_ftp_password" value="<?php echo $dcs_dropship_ftp_password; ?>" size="64"></td></tr>
+		<tr><td><?php _e("Dropship Shopping Cart Page" ); ?></td><td><input type="text" name="dcs_dropship_shopping_cart_page" value="<?php echo $dcs_dropship_shopping_cart_page; ?>" size="64"></td></tr>
+		<tr><td><?php _e("Markup (in percentage)" ); ?></td><td><input type="text" name="dcs_dropship_markup" value="<?php echo $dcs_dropship_markup; ?>" size="2">%</td></tr> 
+		<!-- <tr><td><?php _e(": " ); ?></td><td><input type="text" name="dcs_dropship_" value="<?php echo $dcs_dropship_; ?>" size="128"></td></tr> -->
+		</table>
 		<p class="submit">
 		<input type="submit" name="Submit" value="<?php _e('Update Options', 'dcs_dropship_trdom' ) ?>" />
 		</p>
